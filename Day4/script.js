b71cb03b71f405242124162f9aacfee3f6185437
unknown <aliogutcen@gmail.com>
2023-02-01 02:03:14 +0300

    day-5

--- Day4/script.js
+++ Day4/script.js
@@ -1,57 +1,63 @@
-let secretValues = Math.floor(Math.random() * 10 + 11);
+let secretValues = Math.floor(Math.random() * 20 + 1);
+console.log(secretValues);
 
 let scoreHigh;
+
 document.querySelector(".check").addEventListener("click", function () {
-  const guess = Number(document.querySelector(".guess").value);
-  let scoreHigh = Number(document.querySelector(".score").textContent);
+  let guess = Number(document.querySelector(".guess").value);
+  scoreHigh = Number(document.querySelector(".score").textContent);
+
   if (!guess) {
-    document.querySelector(".message").textContent = "No number";
+    document.querySelector(".message").textContent = " It is empty!!";
 
     if (scoreHigh != 0) {
       scoreHigh = scoreHigh - 1;
       document.querySelector(".score").textContent = String(scoreHigh);
     } else {
-      document.querySelector(".message").textContent =
-        "You are lost the game!!";
+      document.querySelector(".message").textContent = " You're lost the game";
+      document.querySelector("body").style.backgroundColor = "red";
+      document.querySelector(".number").textContent = "!";
     }
-  } else if (guess == secretValues) {
-    document.querySelector(".message").textContent = "Congrats you're won!";
-
-    document.querySelector(".number").textContent = secretValues;
-    document.querySelector("body").style.backgroundColor = "#60b347";
+  } else if (guess === secretValues) {
+    document.querySelector(".message").textContent = " Congrats you're won!";
+    document.querySelector("body").style.backgroundColor = "green";
+    document.querySelector(".number").textContent = String(secretValues);
     document.querySelector(".number").style.width = "30rem";
 
     if (Number(document.querySelector(".highscore").textContent < scoreHigh)) {
-      document.querySelector(".highscore").textContent = scoreHigh;
     }
   } else if (guess > secretValues) {
-    document.querySelector(".message").textContent = "Too High";
+    document.querySelector(".message").textContent = " Too High";
+
     if (scoreHigh != 0) {
       scoreHigh = scoreHigh - 1;
       document.querySelector(".score").textContent = String(scoreHigh);
     } else {
-      document.querySelector(".message").textContent =
-        "You are lost the game!!";
+      document.querySelector(".message").textContent = " You're lost the game";
+      document.querySelector("body").style.backgroundColor = "red";
+      document.querySelector(".number").textContent = "!";
     }
   } else if (guess < secretValues) {
-    document.querySelector(".message").textContent = "Too Low";
+    document.querySelector(".message").textContent = " Too Low";
+
     if (scoreHigh != 0) {
       scoreHigh = scoreHigh - 1;
       document.querySelector(".score").textContent = String(scoreHigh);
     } else {
-      document.querySelector(".message").textContent =
-        "You are lost the game!!";
+      document.querySelector(".message").textContent = " You're lost the game";
+      document.querySelector("body").style.backgroundColor = "red";
+      document.querySelector(".number").textContent = "!";
     }
   }
 });
 
 document.querySelector(".again").addEventListener("click", function () {
   document.querySelector(".score").textContent = "20";
-
-  secretValues = Math.floor(Math.random() * 10 + 11);
+  secretValues = Math.floor(Math.random() * 20 + 1);
+  console.log(secretValues);
   document.querySelector(".message").textContent = "Start guessing!";
-  document.querySelector(".guess").value = "";
-  document.querySelector(".number").textContent = "?";
   document.querySelector("body").style.backgroundColor = "#222";
   document.querySelector(".number").style.width = "15rem";
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
 });
---
+++ Day5/script.js
@@ -0,0 +1,5 @@
+document.querySelector(".btn-primary").addEventListener("click", function () {
+  if (Number(document.querySelector(".form-check-label").get) === 1) {
+    console.log("Kadını seçtiniz");
+  }
+});
